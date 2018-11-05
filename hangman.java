53beb621dfc9ad278c57e8ee432fee8b11579bc9
sedzjad <31769691+sedzjad@users.noreply.github.com>
2018-11-05 21:06:10 +0100

    Add files via upload
    Fixed the Hangman issue

--- hangman.java
+++ hangman.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class hangman {
     public static void main (String[]args){
-        Scanner scanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in);
         StringBuffer buffer = new StringBuffer();
         StringBuffer buffer2 = new StringBuffer();
         String wordGuess;
@@ -10,9 +10,10 @@ public class hangman {
         int livesTotal = 7;
         int lettersLeft;
         boolean wordLetters;
-        char letter;
+        char latters;
         StringBuffer lettersErvoor;
         int positie;
+        boolean playing = true;
         //----------------------------------------------------
 
         String[] stringLijst = new String[]{"Russia","China","Canada","Germany","Liquid","Communism","Capitalism","AlahuAkbar"};
@@ -30,34 +31,33 @@ public class hangman {
         System.out.println(buffer.toString());
         //------------------------------------------------------
 
-        while (lettersLeft > 0 && livesTotal < 7){
-        System.out.println("Take a guess!");
-        letter = scanner.findWithinHorizon(".",0).charAt(0);
+        while (lettersLeft > 0 && livesTotal <= 7){
+                System.out.println("Take a guess!");
+                latters = myScanner.findWithinHorizon(".",0).charAt(0);
 
-        wordLetters = (wordGuess.indexOf(letter))!= -1;
+                wordLetters = (wordGuess.indexOf(latters))!= -1;
 
-        if (wordLetters == false){
-            livesLost++;
-            System.out.println("Sorry, you have lost a life.");
-            System.out.println(livesTotal - livesLost + " left");
-        } else {
-            System.out.println("That was correct!");
-
-            for (positie = 0; positie<wordLenght; positie++){
-                if (wordGuess.charAt(positie) == letter){
-                    System.out.println(letter);
-                    lettersLeft--;
+                if (wordLetters == false){
+                    livesLost++;
+                    System.out.println("Sorry, you have lost a life.");
+                    System.out.println(livesTotal - livesLost + " left");
                 } else {
-                    System.out.println("_ ");
-                }
-            }
-        }
+                    System.out.println("That was correct!");
 
-            lettersErvoor = buffer2.append(letter);
-            System.out.println("Letters guessed before: ");
-            System.out.println(lettersErvoor);
-            System.out.println("Letters remaining: ");
-            System.out.println(lettersLeft);
+                    for (positie = 0; positie<wordLenght; positie++){
+                        if (wordGuess.charAt(positie) == latters){
+                            System.out.println(latters);
+                            lettersLeft--;
+                        } else {
+                            System.out.println("_ ");
+                        }
+                    }
+                }
+                lettersErvoor = buffer2.append(latters);
+                System.out.println("Letters guessed before: ");
+                System.out.println(lettersErvoor);
+                System.out.println("Letters remaining: ");
+                System.out.println(lettersLeft);
         }
         //----------------------------------------------------------------------
         if (livesLost == livesTotal){
