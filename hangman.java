17cf6a7180927799d26edd40678dcc4e9970bae1
sedzjad <31769691+sedzjad@users.noreply.github.com>
2018-11-05 19:41:09 +0100

    Add files via upload
    created a string with random words in it
    made a word randomizer so that it picks random words from the string
    made a function to generate _ for every letter the word has.

--- hangman.java
+++ hangman.java
@@ -12,9 +12,22 @@ public class hangman {
         boolean wordLetters;
         char letter;
         StringBuffer lettersErvoor;
+        int positie;
         //----------------------------------------------------
 
-        
+        String[] stringLijst = new String[]{"Russia","China","Canada","Germany","Liquid","Communism","Capitalism","AlahuAkbar"};
+
+        System.out.println("Welcome to my game, Hang Man. You will have to guess the letters in the word. You'll see how many letters there are in a word and you have 7 lives!");
+        wordGuess = stringLijst[(int)(Math.random()* stringLijst.length)];
+        wordLenght = wordGuess.length();
+
+        System.out.println("The word you're guessing has " + wordLenght + " letters in it.");
+        lettersLeft = wordLenght;
+
+        for (positie = 0; positie<wordLenght; positie++){
+            buffer.append("_");
+        }
+        System.out.println(buffer.toString());
 
     }
 }
