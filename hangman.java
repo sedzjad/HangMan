716ce373845a69619bc781d73eb26adda54e8d2d
sedzjad <31769691+sedzjad@users.noreply.github.com>
2018-11-05 19:10:38 +0100

    Add files via upload
    Created the Java Class
    Inserted variables i will need

---
+++ hangman.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class hangman {
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        StringBuffer buffer = new StringBuffer();
+        StringBuffer buffer2 = new StringBuffer();
+        String wordGuess;
+        int wordLenght;
+        int livesLost = 0;
+        int livesTotal = 7;
+        int lettersLeft;
+        boolean wordLetters;
+        char letter;
+        StringBuffer lettersErvoor;
+        //----------------------------------------------------
+
+        
+
+    }
+}
